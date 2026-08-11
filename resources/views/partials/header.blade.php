93d2509073ae7f73371499a517ad1b57c8c8cd27
nymas-9 <nymsrhma@gmail.com>
2026-08-11 14:31:28 +0700

    misahin navbar + mindahin images

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1247,42 +1247,9 @@
   </div>
 </div>
 
-<!-- ================= FLOATING PILL NAVBAR ================= -->
-<nav id="navbar">
-  <div class="nav-inner">
-    <a href="index.html" class="nav-brand">
-      <div class="nav-logo"><img src="{{ asset('images/logo_smkn2.png') }}" alt="Logo SMK Negeri 2 Mojokerto" /></div>
-      <div class="nav-brand-text">
-        <strong>SMK Negeri <em class="num-2">2</em></strong>
-        <span>Kota Mojokerto</span>
-      </div>
-    </a>
-    <ul class="nav-menu" id="navMenu">
-      <li class="nav-item"><a href="index.html" class="nav-link active">Beranda</a></li>
-      <li class="nav-item">
-        <a href="#" class="nav-link">Profil <i class="fas fa-chevron-down"></i></a>
-        <div class="dropdown-menu">
-          <a href="#"><i class="fas fa-history"></i> Sejarah Sekolah</a>
-          <a href="#"><i class="fas fa-eye"></i> Visi &amp; Misi</a>
-          <a href="#"><i class="fas fa-sitemap"></i> Struktur Organisasi</a>
-          <a href="#"><i class="fas fa-certificate"></i> Akreditasi</a>
-          <a href="guru.html"><i class="fas fa-chalkboard-user"></i> Guru &amp; Staf</a>
-          <a href="school-roadmap.html"><i class="fas fa-road"></i> Roadmap Pengembangan</a>
-        </div>
-      </li>
-      <li class="nav-item"><a href="career-roadmap.html" class="nav-link">Program Keahlian</a></li>
-      <li class="nav-item"><a href="#ppdb" class="nav-link">PPDB</a></li>
-      <li class="nav-item"><a href="#berita" class="nav-link">Berita</a></li>
-      <li class="nav-item"><a href="#galeri" class="nav-link">Galeri</a></li>
-      <li class="nav-item"><a href="karya-siswa.html" class="nav-link">Karya Siswa</a></li>
-      <li class="nav-item"><a href="pkl-tracer.html" class="nav-link">PKL &amp; Alumni</a></li>
-      <li class="nav-item"><a href="#ppdb" class="nav-link nav-cta">Daftar PPDB</a></li>
-    </ul>
-    <button class="nav-toggle" id="navToggle" aria-label="Menu">
-      <span></span><span></span><span></span>
-    </button>
-  </div>
-</nav>
+@include('partials.navbar')
+
+
 
 <!-- ================= HERO SECTION ================= -->
 <section class="hero" id="beranda">
@@ -1379,4 +1346,3 @@
     <svg viewBox="0 0 1440 70" preserveAspectRatio="none"><path fill="#f5f9fd" d="M0,40 C240,80 480,0 720,24 C960,48 1200,72 1440,32 L1440,70 L0,70 Z"></path></svg>
   </div>
 </section>
-
